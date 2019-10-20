6cda710098d598f7cac60ee7b27fa707f8919d18
coconut147 <github@resueh.de>
2019-10-20 10:59:28 +0200

    Initial Commit

---
+++ FoodTrackerLegacy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FoodTrackerLegacy
+//
+//  Created by Frederik Heuser on 20.10.19.
+//  Copyright © 2019 coconut147. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
